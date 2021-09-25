eeecc73b8a23ec67eacdbbdca00cfcd502d4d06a
unknown <mistymoni5050@gmail.comgit config --global user.name sangita-das>
2021-09-25 08:24:50 +0600

    add update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -26,12 +26,20 @@ const Cart = (props) => {
     total = total + product.price;
   } */
 
+  const shipping = 15;
+  const tax = (total + shipping) * 10;
+  const grandTotal = total + shipping + tax;
+
+
   return (
     <div>
       <h3>Order Summary</h3>
       <h5>Items Ordered: {props.cart.length}</h5>
       <br />
-      <p>Total: {total}</p>
+      <p>Total: {total.toFixed(2)}</p>
+      <p>Shipping: {shipping}</p>
+      <p>tax: {tax.toFixed(2)}</p>
+      <p>Grand Total: {grandTotal.toFixed(2)}</p>
     </div>
   );
 };
