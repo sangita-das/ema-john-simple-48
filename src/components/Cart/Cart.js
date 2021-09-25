bc316271076447eccc4061bc7e8a4899681552bf
unknown <mistymoni5050@gmail.comgit config --global user.name sangita-das>
2021-09-25 07:45:26 +0600

    update all

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,10 +4,28 @@ import './Cart.css';
 const Cart = (props) => {
   // console.log(props.cart);
   const { cart } = props;
-  let total = 0;
+
+  const totalReducer = (previous, product) => previous + product;
+  const total = cart.reduce(totalReducer, 0);
+
+
+  // previous two line or below single line or third three line all are run as same.
+  // or
+
+
+
+  /*  const total = cart.reduce((previous, product) => previous + product.price, 0)
+  */
+
+
+  // or
+
+
+  /* let total = 0;
   for (const product of cart) {
     total = total + product.price;
-  }
+  } */
+
   return (
     <div>
       <h3>Order Summary</h3>
