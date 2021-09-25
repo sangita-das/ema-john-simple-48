8a87c2546e967c920b3de968d6033245c69a5c37
unknown <mistymoni5050@gmail.comgit config --global user.name sangita-das>
2021-09-25 08:48:58 +0600

    update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,9 +5,16 @@ const Cart = (props) => {
   // console.log(props.cart);
   const { cart } = props;
 
-  const totalReducer = (previous, product) => previous + product;
-  const total = cart.reduce(totalReducer, 0);
 
+  let totalQuantity = 0;
+  let total = 0;
+  for (const product of cart) {
+    if (!product.quantity) {
+      product.quantity = 1;
+    }
+    total = total + product.price * product.quantity;
+    totalQuantity = totalQuantity + product.quantity;
+  }
 
   // previous two line or below single line or third three line all are run as same.
   // or
@@ -21,20 +28,17 @@ const Cart = (props) => {
   // or
 
 
-  /* let total = 0;
-  for (const product of cart) {
-    total = total + product.price;
-  } */
+  /*   const totalReducer = (previous, product) => previous + product;
+    const total = cart.reduce(totalReducer, 0); */
 
-  const shipping = 15;
-  const tax = (total + shipping) * 10;
+
+  const shipping = total > 0 ? 15 : 0;
+  const tax = (total + shipping) * 0.10;
   const grandTotal = total + shipping + tax;
-
-
   return (
     <div>
       <h3>Order Summary</h3>
-      <h5>Items Ordered: {props.cart.length}</h5>
+      <h5>Items Ordered: {totalQuantity}</h5>
       <br />
       <p>Total: {total.toFixed(2)}</p>
       <p>Shipping: {shipping}</p>
